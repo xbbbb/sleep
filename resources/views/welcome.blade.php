5b720c4c8d879547a13cebd04e60048aea5e9665
xbbbb <810610025wu@gmail.com>
2021-02-25 14:13:36 +0800

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Sleep</title>
+        <title>Sheep</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -70,7 +69,6 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
-
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
@@ -79,8 +77,11 @@
             @endif
 
             <div class="content">
+                <div>
+                    <img src="{{url("/img/logo.png")}}">
+                </div>
                 <div class="title m-b-md">
-                   Sleep
+                   Sheep
                 </div>
 
 
