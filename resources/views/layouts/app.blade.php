cab8e3b04161e7e3c83262a55d1c0fb2626e3e3d
xbbbb <810610025wu@gmail.com>
2021-03-08 21:08:55 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,8 @@
                                 @endif
                             </li>
                         @else
-                           <li><a class="nav-link " href="{{ url('/record') }}" style="font-size: 16px">{{ __('Sleeping Record') }}</a></li>
+                            <li><a class="nav-link " href="{{ url('/home') }}" style="font-size: 16px">{{ __('Controller Panel') }}</a></li>
+                            <li><a class="nav-link " href="{{ url('/record') }}" style="font-size: 16px">{{ __('Sleeping Record') }}</a></li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
