2a8df17477b84e61abf2b20c43c9ae830f92d856
xbbbb <810610025wu@gmail.com>
2021-05-06 13:49:10 +0800

    Update login.blade.php

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Basic Info') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ action('UserController@user_login') }}">
@@ -40,17 +40,7 @@
                         </div>
 
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
